ff6f9a54875a2f8382be0dcdc26fd2e79255411a
Antonio <angelesblazquez.antonio@jgvaldemora.org>
2025-09-24 10:49:50 +0200

    primer commit

---
+++ src/test/java/com/diw/practicadiw/PracticadiwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diw.practicadiw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticadiwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
