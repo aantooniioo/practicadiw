ab6182a8fccfd482522c0ebb2e933a17c677bceb
Antonio <angelesblazquez.antonio@jgvaldemora.org>
2025-10-30 13:17:11 +0100

    Hola

--- dao/LibroRepository.java
+++ dao/LibroRepository.java
@@ -1,6 +1,5 @@
-package com.diw.practica.repository;
+package com.diw.practica.dao;
 
-import java.util.List;
 import com.diw.practica.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
